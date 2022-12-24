501dc9f555f9222e6a1e68b4232355c3f3e6219e
azka <mohazkalarif"gmail.com>
2022-12-25 03:25:43 +0700

    add meta mobile device

--- app/head.jsx
+++ app/head.jsx
@@ -2,6 +2,10 @@ export default function Head({ params }) {
     return (
         <>
             <title>Torao by Azkal</title>
+            <meta
+                name='viewport'
+                content='width=device-width, initial-scale=1.0'
+            />
         </>
     )
 }
